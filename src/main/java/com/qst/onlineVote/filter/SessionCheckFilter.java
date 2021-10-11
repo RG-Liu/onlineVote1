dcfafdcb1b0d6ab3cd6ae4fce58692ab30c9bd19
Liurg <626560344@qq.com>
2021-10-11 08:35:34 +0800

    2021/10/11

--- src/main/java/com/qst/onlineVote/filter/SessionCheckFilter.java
+++ src/main/java/com/qst/onlineVote/filter/SessionCheckFilter.java
@@ -1,6 +1,5 @@
 package com.qst.onlineVote.filter;
 
-
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -18,47 +17,50 @@ public class SessionCheckFilter implements Filter {
 	public SessionCheckFilter() {
 
 	}
-	 //  销毁时调用
+
+	// 销毁时调用
 	public void destroy() {
 	}
-	
-	 //过滤方法 主要是对request和response进行一些处理，然后交给下一个过滤器或Servlet处理
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException { //执行过滤
+
+	// 过滤方法 主要是对request和response进行一些处理，然后交给下一个过滤器或Servlet处理
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException { // 执行过滤
 		request.setCharacterEncoding("utf-8");
-    	response.setContentType("text/html;charset=utf-8");
+		response.setContentType("text/html;charset=utf-8");
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();
-//		 String path=req.getRequestURI();	
-//	 System.out.println(path);
-//		 boolean flag= path.equals("/onlineVote/LoginPage.jsp")||path.equals("/onlineVote/code")
-//			 ||path.equals("/onlineVote/admin/reg.jsp")||path.equals("/onlineVote/admin/regS.jsp")
-//			  ||path.equals("/onlineVote/AjaxRegisterServlet")||path.equals("/onlineVote/registerServlet");
-//
-//		  if(flag){
-//			  
-//		   chain.doFilter(request, response);
-// }	 else{
-//	 Object obj=req.getSession().getAttribute("useruser");
-//	 if(obj==null){
-//		 response.getWriter().print("<script language='JavaScript'>alert('请先登录!');window.location.href='../LoginPage.jsp';</script>");
-//		
-//	 }else{
-//		 chain.doFilter(request, response);
-//	 }
-//   }
-		
-		 	  if (session.getAttribute("username") != null) {
-				chain.doFilter(request, response);//交给下一个过滤器或servlet处理
+		String path = req.getRequestURI();
+		System.out.println(path);
+		if (session.getAttribute("username") != null) {
+			chain.doFilter(request, response);// 交给下一个过滤器或servlet处理
+		} else {
+			boolean flag = path.equals("/onlineVote/LoginPage.jsp")
+					|| path.equals("/onlineVote/code")
+					|| path.equals("/onlineVote/admin/reg.jsp")
+					|| path.equals("/onlineVote/admin/regS.jsp")
+					|| path.equals("/onlineVote/AjaxRegisterServlet")
+					|| path.equals("/onlineVote/registerServlet")
+					|| path.equals("/onlineVote/login_files/login.css")
+					|| path.equals("/onlineVote/admin/reg_files/reg.css")
+					|| path.equals("/onlineVote/admin/reg_files/logo.gif")
+					|| path.equals("/onlineVote/admin/reg_files/jquery-1.x.js.%E4%B8%8B%E8%BD%BD")
+					|| path.equals("/onlineVote/registerServlet")
+					|| path.equals("/onlineVote/admin/regS.jsp")
+					|| path.equals("/onlineVote/admin/regS_files/reg.css")
+					|| path.equals("/onlineVote/admin/regS_files/logo.gif")
+					|| path.equals("/onlineVote/LoginServlet");
+			if (flag) {
+				chain.doFilter(request, response);
 			} else {
-				
-				response.getWriter().print("<script language='JavaScript'>alert('请先登录!');window.location.href='../LoginPage.jsp';</script>");
-			
-			}
-		  	
+				response.getWriter()
+						.print("<script language='JavaScript'>alert('请先登录!');window.location.href='/onlineVote/LoginPage.jsp';</script>");
+			}			
+		}
+		//
 
 	}
-	//初始化方法，对象创建后init方法开始执行，且只执行一次；主要功能初始化资源的
+
+	// 初始化方法，对象创建后init方法开始执行，且只执行一次；主要功能初始化资源的
 	public void init(FilterConfig fConfig) throws ServletException {
 
 	}
